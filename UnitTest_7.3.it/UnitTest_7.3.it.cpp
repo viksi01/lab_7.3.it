41f6122013498ee2f0c58d6cf183485982fd919c
viksi01 <victoria.pyatnicka@gmail.com>
2022-12-02 01:07:29 +0200

    commit1_7.3.it

---
+++ UnitTest_7.3.it/UnitTest_7.3.it.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_7.3.it/lab_7.3.it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest73it
+{
+	TEST_CLASS(UnitTest73it)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+            int result;
+            int a[3][3] = {
+              {1, 5, 8},
+              {2, 15, 7},
+              {3, -7, 9},
+            };
+            int* pa[3] = { a[0], a[1], a[2] };
+            result = Sum(pa, 3, 3); ;
+            Assert::AreEqual(result, 38);
+
+		}
+	};
+}
---
+++ lab_7.3.it/lab_7.3.it.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+void Create(int** a, const int rowCount, const int colCount, const int Low,
+    const int High)
+{
+    for (int i = 0; i < rowCount; i++)
+        for (int j = 0; j < colCount; j++)
+            a[i][j] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int** a, const int rowCount, const int colCount)
+{
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            cout << a[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+int Sum(int** a, const int rowCount, const int colCount)
+{
+    int s = 0, s1 = 0;
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            if (a[i][j] >= 0)
+                s1 += a[i][j];
+
+            else
+            {
+                s1 = 0;
+                break;
+            }
+        }
+
+        s += s1;
+
+        s1 = 0;
+    }
+
+    return s;
+}
+
+
+int main()
+{
+    srand(time(NULL));
+
+    const int High = 100;
+    const int Low = -10;
+    int rowCount;
+    int colCount;
+    cout << "Enter the matrix size: "; cin >> rowCount;
+    colCount = rowCount;
+
+    int** a = new int* [rowCount];
+    for (int i = 0; i < rowCount; i++)
+        a[i] = new int[colCount];
+
+    Create(a, rowCount, colCount, Low, High);
+    Print(a, rowCount, colCount);
+    cout << "Sum = " << Sum(a, rowCount, colCount);
+
+    for (int i = 0; i < rowCount; i++)
+        delete[] a[i];
+    delete[]a;
+
+    return 0;
+}
